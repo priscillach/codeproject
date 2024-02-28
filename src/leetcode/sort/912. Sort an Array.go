233e2642acb27a356ff6f1518137920e07bbb581
Steven Wen <wzq961021@gmail.com>
2024-02-29 00:29:23 +0800

    feat: sort

---
+++ src/leetcode/sort/912. Sort an Array.go
@@ -0,0 +1,36 @@
+package sort
+
+import "math/rand"
+
+func sortArray(nums []int) []int {
+	sort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func sort(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+	cur := fastSort(nums, left, right)
+	sort(nums, left, cur-1)
+	sort(nums, cur+1, right)
+}
+
+func fastSort(nums []int, left, right int) int {
+	randIdx := left + rand.Intn(right-left+1)
+	tmp := nums[randIdx]
+	nums[randIdx] = nums[right]
+	nums[right] = tmp
+	for left < right {
+		for left < right && nums[left] <= tmp {
+			left++
+		}
+		nums[right] = nums[left]
+		for left < right && nums[right] >= tmp {
+			right--
+		}
+		nums[left] = nums[right]
+	}
+	nums[left] = tmp
+	return left
+}

76894826d97c6dd712fa37381c3e3b526cbde313
Steven Wen <501924917@qq.com>
2024-03-09 00:12:09 +0800

    feat: arrange category

---
+++ src/utils/math_helper.go
@@ -0,0 +1,23 @@
+package utils
+
+import "math"
+
+func Max(nums ...int) int {
+	max := math.MinInt32
+	for i := range nums {
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
+func Min(nums ...int) int {
+	min := math.MaxInt32
+	for i := range nums {
+		if nums[i] < min {
+			min = nums[i]
+		}
+	}
+	return min
+}

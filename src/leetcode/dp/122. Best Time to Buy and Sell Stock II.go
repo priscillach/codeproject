097f86b44765da2032e436a8a179fe6c65158096
Steven Wen <501924917@qq.com>
2024-03-22 10:13:40 +0800

    feat: stock profit

--- src/leetcode/dp/122. Best Time to Buy and Sell Stock II.go
+++ src/leetcode/dp/122. Best Time to Buy and Sell Stock II.go
@@ -10,3 +10,18 @@ func maxProfit(prices []int) int {
 	}
 	return cash
 }
+
+func maxProfitV2(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	profit := 0
+	prices = append(prices, 0)
+	for i := 1; i < len(prices); i++ {
+		if prices[i] <= prices[i-1] {
+			continue
+		}
+		profit += prices[i] - prices[i-1]
+	}
+	return profit
+}

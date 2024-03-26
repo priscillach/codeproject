9e19cc28b53b06f6015a4c90986d3d3a651f5e35
Steven Wen <501924917@qq.com>
2024-03-26 22:36:03 +0800

    feat: merge intervals

---
+++ src/leetcode/greedy/56. Merge Intervals.go
@@ -0,0 +1,22 @@
+package greedy
+
+import (
+	"leetcode/src/utils"
+	"sort"
+)
+
+func mergeIntervals(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	res := [][]int{intervals[0]}
+
+	for idx := range intervals {
+		if res[len(res)-1][1] < intervals[idx][0] {
+			res = append(res, intervals[idx])
+			continue
+		}
+		res[len(res)-1][1] = utils.Max(res[len(res)-1][1], intervals[idx][1])
+	}
+	return res
+}

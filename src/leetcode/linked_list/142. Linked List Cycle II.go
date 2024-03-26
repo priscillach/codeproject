aaeb21de23c4cc14bc0849b052d4c29d92c78d6c
Steven Wen <501924917@qq.com>
2024-03-26 18:48:15 +0800

    feat: detect cycle

---
+++ src/leetcode/linked_list/142. Linked List Cycle II.go
@@ -0,0 +1,24 @@
+package linked_list
+
+import "leetcode/src/define/mylinkednode"
+
+func detectCycle(head *mylinkednode.ListNode) *mylinkednode.ListNode {
+	var hasCycleFlag bool
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			hasCycleFlag = true
+			break
+		}
+	}
+	if !hasCycleFlag {
+		return nil
+	}
+	for head != slow {
+		head = head.Next
+		slow = slow.Next
+	}
+	return head
+}

5908b69989f1e4b0f31c1930b10a424b9d2f50e9
Steven Wen <501924917@qq.com>
2024-04-10 14:12:00 +0800

    feat: min path sum

---
+++ src/leetcode/dp/64. Minimum Path Sum.go
@@ -0,0 +1,24 @@
+package dp
+
+import "leetcode/src/utils"
+
+func minPathSum(grid [][]int) int {
+	dp := make([][]int, len(grid))
+	for i := 0; i < len(grid); i++ {
+		dp[i] = make([]int, len(grid[0]))
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = grid[i][j]
+			} else if i == 0 {
+				dp[i][j] = grid[i][j] + dp[i][j-1]
+			} else if j == 0 {
+				dp[i][j] = grid[i][j] + dp[i-1][j]
+			} else {
+				dp[i][j] = utils.Min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[len(grid)-1][len(grid[0])-1]
+}
--- src/tool/leetcode_trans_test.go
+++ src/tool/leetcode_trans_test.go
@@ -8,22 +8,27 @@ import (
 
 func TestTransLCP(t *testing.T) {
 	transCode := `
-var diameterOfBinaryTreeMax int
-func diameterOfBinaryTree(root *mytreenode.TreeNode) int {
-	diameterOfBinaryTreeMax = 0
-	diameterOfBinaryTreeCore(root)
-	return diameterOfBinaryTreeMax
+func minPathSum(grid [][]int) int {
+	dp := make([][]int, len(grid))
+	for i := 0; i < len(grid); i++ {
+		dp[i] = make([]int, len(grid[0]))
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = grid[i][j]
+			} else if i == 0 {
+				dp[i][j] = grid[i][j] + dp[i][j - 1]
+			} else if j == 0 {
+				dp[i][j] = grid[i][j] + dp[i - 1][j]
+			} else {
+				dp[i][j] = utils.Min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+			}
+		}
+	}
+	return dp[len(grid) - 1][len(grid[0]) - 1]
 }
 
-func diameterOfBinaryTreeCore(root *mytreenode.TreeNode) int {
-	if root == nil {
-		return 0
-	}
-	depthLeft := diameterOfBinaryTree(root.Left)
-	depthRight := diameterOfBinaryTree(root.Right)
-	diameterOfBinaryTreeMax = utils.Max(diameterOfBinaryTreeMax, depthLeft + depthRight)
-	return utils.Max(depthLeft, depthRight) + 1
-}
 `
 	transCode = strings.ReplaceAll(transCode, "mytreenode.", "")
 	transCode = strings.ReplaceAll(transCode, "mylinkednode.", "")

0ca66467c017445fd45c1e4683fcea51ed5e91c2
Steven Wen <501924917@qq.com>
2024-04-11 22:51:22 +0800

    feat: decode string

---
+++ src/leetcode/stack/394. Decode String.go
@@ -0,0 +1,31 @@
+package stack
+
+import (
+	"strings"
+)
+
+func decodeString(s string) string {
+	strStack := []string{""}
+	var numStack []int
+	var num int
+	for i := 0; i < len(s); i++ {
+		if s[i] <= 'z' && s[i] >= 'a' {
+			strStack[len(strStack)-1] += string(s[i])
+		} else if s[i] != '[' && s[i] != ']' {
+			num = num*10 + int(s[i]-'0')
+		} else if s[i] == '[' {
+			numStack = append(numStack, num)
+			// add a new empty str into stack as encountering a new sub "[...]"
+			strStack = append(strStack, "")
+			num = 0
+		} else {
+			str := strStack[len(strStack)-1]
+			strStack = strStack[:len(strStack)-1]
+			curNum := numStack[len(numStack)-1]
+			numStack = numStack[:len(numStack)-1]
+			// add the repeats to the last str in the stack
+			strStack[len(strStack)-1] += strings.Repeat(str, curNum)
+		}
+	}
+	return strStack[0]
+}
---
+++ src/leetcode/stack/func_test.go
@@ -0,0 +1,7 @@
+package stack
+
+import "testing"
+
+func TestDecodeString(t *testing.T) {
+	decodeString("2[abc]3[cd]ef")
+}

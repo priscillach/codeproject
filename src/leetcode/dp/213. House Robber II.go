eb34c3c2aad9fa19d7d0a3d1d2d2e47c1eb7ffbe
Steven Wen <501924917@qq.com>
2024-04-20 01:10:02 +0800

    feat: rob house

--- src/leetcode/dp/213. House Robber II.go
+++ src/leetcode/dp/213. House Robber II.go
@@ -6,19 +6,5 @@ func rob213(nums []int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
-	return utils.Max(robCore213(nums[1:]), robCore213(nums[:len(nums)-1]))
-}
-
-func robCore213(nums []int) int {
-	picked, unPicked := 0, 0
-	for i := 0; i < len(nums); i++ {
-		if i == 0 {
-			picked = nums[i]
-			continue
-		}
-		tmp := unPicked + nums[i]
-		unPicked = utils.Max(picked, unPicked)
-		picked = tmp
-	}
-	return utils.Max(unPicked, picked)
+	return utils.Max(rob198(nums[1:]), rob198(nums[:len(nums)-1]))
 }

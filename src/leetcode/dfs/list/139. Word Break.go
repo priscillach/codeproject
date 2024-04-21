c80e826cec44357e06949b0b9970ec89b15d4aaf
Steven Wen <501924917@qq.com>
2024-04-21 22:46:28 +0800

    feat: work break

---
+++ src/leetcode/dfs/list/139. Word Break.go
@@ -0,0 +1,28 @@
+package list
+
+func wordBreak(s string, wordDict []string) bool {
+	dict := make(map[string]struct{})
+	for _, word := range wordDict {
+		dict[word] = struct{}{}
+	}
+	cantSegmented := make([]bool, len(s))
+	return wordBreakCore(s, 0, cantSegmented, dict)
+}
+
+func wordBreakCore(s string, i int, cantSegmented []bool, dict map[string]struct{}) bool {
+	if i == len(s) {
+		return true
+	}
+	if cantSegmented[i] {
+		return false
+	}
+	for j := i + 1; j <= len(s); j++ {
+		if _, ok := dict[s[i:j]]; ok {
+			if wordBreakCore(s, j, cantSegmented, dict) {
+				return true
+			}
+		}
+	}
+	cantSegmented[i] = true
+	return false
+}
--- src/leetcode/sort/func_test.go
+++ src/leetcode/sort/func_test.go
@@ -11,6 +11,12 @@ func TestBubbleSort(t *testing.T) {
 	fmt.Println(nums)
 }
 
+func TestHeapSort(t *testing.T) {
+	nums := []int{5, 2, 1, 5, 4, 2, 3, 1}
+	HeapSort(nums)
+	fmt.Println(nums)
+}
+
 func TestLargestNumber(t *testing.T) {
 	fmt.Println(largestNumber([]int{34323, 3432}))
 }

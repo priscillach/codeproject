ee65a4ae4d2810a2d8dd856ffb49b8f74d07838a
Steven Wen <501924917@qq.com>
2024-09-16 13:29:57 +0800

    feat: 539. Minimum Time Difference.go

---
+++ src/leetcode/sort/539. Minimum Time Difference.go
@@ -0,0 +1,35 @@
+package _sort
+
+import (
+	"leetcode/src/utils/mathhelper"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func findMinDifference(timePoints []string) int {
+	sort.Strings(timePoints)
+	minDistance := math.MaxInt
+	for i := 0; i < len(timePoints); i++ {
+		if i == len(timePoints)-1 {
+			minDistance = mathhelper.Min(minDistance, computeMinDifference(timePoints[0], timePoints[i]))
+			continue
+		}
+		minDistance = mathhelper.Min(minDistance, computeMinDifference(timePoints[i], timePoints[i+1]))
+	}
+	return minDistance
+}
+
+func computeMinDifference(time1, time2 string) int {
+	time1Slice := strings.Split(time1, ":")
+	time2Slice := strings.Split(time2, ":")
+	t1Hour, _ := strconv.Atoi(time1Slice[0])
+	t1Min, _ := strconv.Atoi(time1Slice[1])
+	t2Hour, _ := strconv.Atoi(time2Slice[0])
+	t2Min, _ := strconv.Atoi(time2Slice[1])
+	time1Sum := t1Hour*60 + t1Min
+	time2Sum := t2Hour*60 + t2Min
+	dis := int(mathhelper.Abs(int(time1Sum) - int(time2Sum)))
+	return mathhelper.Min(dis, 24*60-dis)
+}
